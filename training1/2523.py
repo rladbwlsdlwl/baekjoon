52780da91d8678f2f2004ab51a04ec69a3efb2ab
rladbwlsdlwl <rladbwlsdlwl@naver.com>
2020-07-15 01:34:33 +0900

    python3

---
+++ training1/2523.py
@@ -0,0 +1,5 @@
+n=int(input())
+for i in range(n):
+   print('*'*(i+1))
+for j in range(n-1):
+    print('*'*(n-j-1))
---
+++ training1/5543.py
@@ -0,0 +1,13 @@
+import sys
+input=sys.stdin.readline
+
+burger=[]
+drink=[]
+
+for i in range(3):
+    burger.append(int(input()))
+
+for j in range(2):
+    drink.append(int(input()))
+
+print(min(burger)+min(drink)-50)
