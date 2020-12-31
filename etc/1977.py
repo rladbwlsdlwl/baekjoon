75b276ebef76562343ee54d7ed9d0d2823aad362
rladbwlsdlwl <rladbwlsdlwl@naver.com>
2020-12-31 18:58:31 +0900

    python solution

--- etc/1977.py
+++ etc/1977.py
@@ -1,4 +1,4 @@
-table=[ for i in range(1,101)] #push number between 1 and 10000
+table=[i*i for i in range(1,101)] #push number between 1 and 10000
 result=[i for i in range(int(input()),int(input())+1) if(i in table)]
 print(str(sum(result))+'\n'+str(result[0]) if(len(result)) else -1)
 
