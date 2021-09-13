bf2fa40090713e642bbb5359f6b3d2bdd81ed614
rladbwlsdlwl <rladbwlsdlwl@naver.com>
2021-09-13 18:50:57 +0900

    python solution(Silver III, backtracking)

---
+++ backtracking/15652_NAndM4.py
@@ -0,0 +1,29 @@
+import sys
+input=sys.stdin.readline
+
+class Solution:
+    def __init__(self,n=0,m=0):
+        self.n=n
+        self.m=m
+        self.numArray=[i+1 for i in range(n)]
+        self.temp=[]
+
+    def get_answer(self):
+        self.dfs(self.m)
+
+    def dfs(self,m):
+        if m==0:
+            print(*self.temp, end=" ")
+            print()
+        else:
+            for i in range(self.n):
+                if len(self.temp) and self.temp[-1]>self.numArray[i]:
+                    continue 
+                self.temp.append(self.numArray[i])
+                self.dfs(m-1)
+                self.temp.pop()
+
+
+N,M=map(int,input().split())
+s=Solution(N,M)
+s.get_answer()
