109e0b52e81c43da45729e1839e8b4290694083b
rladbwlsdlwl <rladbwlsdlwl@naver.com>
2021-10-05 20:11:19 +0900

    c++ solution(Silver III, dp)

---
+++ 1463_MakeBy1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+int dp[1000002];
+
+int main() {
+	int n;
+	cin >> n;
+
+	dp[0] = dp[1] = 0;
+	dp[2] = dp[3] = 1;
+
+	for (int i = 4; i <= n; i++) {
+		if (i % 2 == 0 && i % 3 == 0) {
+			dp[i] = min(dp[i / 3], dp[i / 2]);
+			dp[i] = min(dp[i], dp[i - 1]) + 1;
+		}
+		else if (i % 2 == 0)
+			dp[i] = min(dp[i / 2], dp[i - 1]) + 1;
+		else if (i % 3 == 0)
+			dp[i] = min(dp[i / 3], dp[i - 1]) + 1;
+		else
+			dp[i] = dp[i - 1] + 1;
+	}
+
+	cout << dp[n] << "\n";
+	return 0;
+}
